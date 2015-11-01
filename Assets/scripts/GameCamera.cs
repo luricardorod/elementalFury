6128b9997f0f4a0e1ad11837fdde95a13b4d3c90
pugakn <puga_deathnote@hotmail.com>
2015-11-01 02:33:44 -0600

    Adición de Script de cámara
    Se añadió el script de seguimiento de cámara.

---
+++ Assets/scripts/GameCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+	private Transform player;
+	public float smoothing = 5f;
+
+		void Start () {
+			player = GameObject.Find ("Charly").transform;
+		}
+
+	     void FixedUpdate() {
+	         if (player) {
+	             var playerPos = player.position;
+	             Vector3 trackPos = new Vector3(playerPos.x, playerPos.y, -10);
+							 transform.position = Vector3.Lerp (transform.position, trackPos, smoothing * Time.deltaTime);
+	         }
+	     }
+}
